070cef79ed79ee0f97ae2267cef7c204dd8098e3
Aaron Fleck <revolverwest@gmail.com>
2023-05-11 08:11:22 -0600

    Update I_Dont_Know_What_Im_Doing.js
    add the ability to read the first argument of a script.

--- I_Dont_Know_What_Im_Doing.js
+++ I_Dont_Know_What_Im_Doing.js
@@ -1,21 +1,23 @@
 /** @param {NS} ns */
 export async function main(ns) {
-var target = "n00dles";
+    var target = ns.args[0];
 
-while(true) {
     // Find the threasholds of the server.
-    var moneyThresh = ns.getServerMaxMoney(target) * 0.75;
-    var securityThresh = ns.getServerMinSecurityLevel(target) + 5;
-    
-    if (ns.getServerSecurityLevel(target) > securityThresh) {
-        // If the server's security level is above our threshold, weaken it
-        await ns.weaken(target);
-    } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
-        // If the server's money is less than our threshold, grow it
-        await ns.grow(target);
-    } else {
-        // Otherwise, hack it
-        await ns.hack(target);
+    var moneyThresh = ns.getServerMaxMoney(target) * 0.9;
+    var securityThresh = ns.getServerMinSecurityLevel(target) + .5;
+
+    while (true) {
+
+
+        if (ns.getServerSecurityLevel(target) > securityThresh) {
+            // If the server's security level is above our threshold, weaken it
+            await ns.weaken(target);
+        } else if (ns.getServerMoneyAvailable(target) < moneyThresh) {
+            // If the server's money is less than our threshold, grow it
+            await ns.grow(target);
+        } else {
+            // Otherwise, hack it
+            await ns.hack(target);
+        }
     }
-}
 }
